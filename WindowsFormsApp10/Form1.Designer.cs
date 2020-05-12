f86e250445746cbf37178fa0eda485ea106a0c5f
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-12 19:20:45 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rez = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Num
+            // 
+            this.Num.Location = new System.Drawing.Point(165, 42);
+            this.Num.Name = "Num";
+            this.Num.Size = new System.Drawing.Size(246, 22);
+            this.Num.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число n: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(36, 92);
+            this.rez.Multiline = true;
+            this.rez.Name = "rez";
+            this.rez.ReadOnly = true;
+            this.rez.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.rez.Size = new System.Drawing.Size(667, 470);
+            this.rez.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(561, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Очистить ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 574);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rez);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Num);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Num;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox rez;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void N(int n, int a)
+        {
+            if (a <= n)
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    rez.Text += a + " ";                
+                }
+                rez.Text += Environment.NewLine;
+                N(n, a + 1);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int n = Convert.ToInt32(Num.Text);
+                if (n > 0)
+                { N(n, 1); }
+                else rez.Text = Convert.ToString("Число должно быть больше 0!");
+            }
+            catch { rez.Text = Convert.ToString("Некорректный ввод!"); }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rez.Clear();
+            Num.Clear();
+        }
+    }
+}
